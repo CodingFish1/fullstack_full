617a4defba05b5561e45fcf2749e8e81dfdcd59d
CodingFisher <cppmaster@outlook.com>
2022-06-13 23:06:24 +0930

    new

--- controller/posts.js
+++ controller/posts.js
@@ -151,14 +151,14 @@ const posts = {
     async getCommentsByUser (req, res, next) {
         const user = req.params.id;
         const result = await Post.find({ user })
-        .populate({
-          path: "user",
-          select: "name avatar",
-        })
-        .populate({
-          path: "comments",
-          select: "comment user createdAt",
-        });
+        // .populate({
+        //   path: "user",
+        //   select: "name avatar",
+        // })
+        // .populate({
+        //   path: "comments",
+        //   select: "comment user createdAt",
+        // });
         successHandler(res,result);
     }
 }
