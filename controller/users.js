fe7bbd6dd60c37d6aecc0e97ada8a95c97683b7f
CodingFisher <cppmaster@outlook.com>
2022-06-14 12:48:22 +0930

    new

--- controller/users.js
+++ controller/users.js
@@ -163,7 +163,7 @@ const users = {
     async getLikeList (req, res, next) {
         const userId = req.user.id
         const result = await Post.find({
-            likes: { $in: userId },
+            likes: { $in: [userId] },
         })
         successHandler(res, result);
     }
